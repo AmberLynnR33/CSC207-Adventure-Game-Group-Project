8541fd378e73feb860f20b1f2b5342ca61c2f2c6
AmberLynnR33 <103320792+AmberLynnR33@users.noreply.github.com>
2023-11-30 19:27:31 -0500

    Fixed merging issue caused by new passed parameter for movePlayer

--- PlayerMovement/ChaoticMovementTests.java
+++ PlayerMovement/ChaoticMovementTests.java
@@ -1,6 +1,7 @@
 package PlayerMovement;
 
 import AdventureModel.AdventureGame;
+import AdventureModel.AdventureGameStatistics;
 import javafx.stage.Stage;
 import org.junit.jupiter.api.*;
 import views.AdventureGameView;
@@ -14,28 +15,30 @@ class ChaoticMovementTests {
     @Test
     void testMove1(){
         AdventureGame model = new AdventureGame("MediumGame");
+        Stage testStage = new Stage();
+        AdventureGameView view = new AdventureGameView(model, testStage);
         model.setMovementGameMode("01");
 
         System.out.println("Normal Movement Rooms: 1 -> 2 -> 1 -> 3 -> 30 -> 36 -> 3");
 
         ArrayList<Integer> pathList = new ArrayList<>();
 
-        model.movePlayer("WEST");
+        model.movePlayer("WEST", view);
         pathList.add(model.player.getCurrentRoom().getRoomNumber());
 
-        model.movePlayer("EAST");
+        model.movePlayer("EAST", view);
         pathList.add(model.player.getCurrentRoom().getRoomNumber());
 
-        model.movePlayer("NORTH");
+        model.movePlayer("NORTH", view);
         pathList.add(model.player.getCurrentRoom().getRoomNumber());
 
-        model.movePlayer("NORTH");
+        model.movePlayer("NORTH", view);
         pathList.add(model.player.getCurrentRoom().getRoomNumber());
 
-        model.movePlayer("FORCED");
+        model.movePlayer("FORCED", view);
         pathList.add(model.player.getCurrentRoom().getRoomNumber());
 
-        model.movePlayer("FORCED");
+        model.movePlayer("FORCED", view);
         pathList.add(model.player.getCurrentRoom().getRoomNumber());
 
         System.out.print("In this Case: 1");
@@ -45,27 +48,31 @@ class ChaoticMovementTests {
 
         //Added so can view the different paths taken
         assertEquals(1, 1);
+
+        testStage.close();
     }
 
     @Test
     void movesForced(){
         AdventureGame model = new AdventureGame("MediumGame");
+        Stage testStage = new Stage();
+        AdventureGameView view = new AdventureGameView(model, testStage);
         model.setMovementGameMode("00");
 
         System.out.println("Rooms Should Go Through: 1 -> 3 -> 30 -> 36 -> 3");
 
         ArrayList<Integer> pathList = new ArrayList<>();
 
-        model.movePlayer("NORTH");
+        model.movePlayer("NORTH", view);
 
-        model.movePlayer("NORTH");
+        model.movePlayer("NORTH", view);
 
         model.setMovementGameMode("01");
 
-        model.movePlayer("FORCED");
+        model.movePlayer("FORCED", view);
         pathList.add(model.player.getCurrentRoom().getRoomNumber());
 
-        model.movePlayer("FORCED");
+        model.movePlayer("FORCED", view);
         pathList.add(model.player.getCurrentRoom().getRoomNumber());
 
         System.out.print("In this Case: 1 -> 3 -> 30");
@@ -76,6 +83,7 @@ class ChaoticMovementTests {
         assertEquals(36, pathList.get(0));
         assertEquals(3, pathList.get(1));
 
+        testStage.close();
     }
 
     @Test
@@ -84,16 +92,19 @@ class ChaoticMovementTests {
 
         for(int i = 0; i < 10; i++){
             AdventureGame model = new AdventureGame("MediumGame");
+            Stage testStage = new Stage();
+            AdventureGameView view = new AdventureGameView(model, testStage);
             model.setMovementGameMode("00");
 
-            model.movePlayer("DOWN");
-            model.movePlayer("DOWN");
-            model.movePlayer("DOWN");
+            model.movePlayer("DOWN", view);
+            model.movePlayer("DOWN", view);
+            model.movePlayer("DOWN", view);
 
             model.setMovementGameMode("01");
 
-            model.movePlayer("DOWN");
+            model.movePlayer("DOWN", view);
             movedTo.add(model.player.getCurrentRoom().getRoomNumber());
+            testStage.close();
         }
 
         for (Integer curMovedTo: movedTo){
