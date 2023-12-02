d7841b843a2f35616055bb062c94cb80ec8c6aa6
saltieboi <aj.cole@mail.utoronto.ca>
2023-12-02 16:45:29 -0500

    comments for npc and dialogue

--- NPC/Dialogue.java
+++ NPC/Dialogue.java
@@ -1,9 +1,28 @@
 package NPC;
 
-public class Dialogue{
+import java.io.Serializable;
+
+/**
+ * This class contains information about a dialogue said by an NPC
+ */
+public class Dialogue implements Serializable {
+    /**
+     * id number for the dialogue.
+     * NPCName + id is used for sound files and must be unique.
+     */
     public int id;
+    /**
+     * name of the NPC
+     */
     public String NPCName;
+    /**
+     * message said to the player
+     */
     public String message;
+    /**
+     * event string indicating when the message is no longer relevent
+     * Used for ProgressionObserver and ProgressionPublisher Interraction
+     */
     public String completionEvent;
 
     public Dialogue(String message, String completionEvent, String NPCName, int id) {
--- NPC/NPC.java
+++ NPC/NPC.java
@@ -3,21 +3,45 @@ package NPC;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
-
+/**
+ * This class contains information about an NPC
+ */
 public class NPC implements ProgressionObserver, Serializable {
+    /**
+     * name of the NPC
+     */
     String name;
-    private List<Dialogue> dialouges; //A priority list of NPC advice. Highest priority is last and Advice is removed if its completionEvent is done.
+    /**
+     * A priority queue of NPC advice. Highest priority is last and Advice is removed if its completionEvent is done.
+     */
+    private List<Dialogue> dialouges;
     public NPC(String name){
         this.name = name;
         this.dialouges = new ArrayList<Dialogue>();
     }
+
+    /**
+     * Update the NPC on the player's progression. Affects what the NPC says.
+     * @param event completion event. e.g. "TAKEN KEYS"
+     */
     public void update(String event){
         dialouges.removeIf(advice -> advice.completionEvent.equals(event));
     }
+
+    /**
+     * Gets the dialogue most relevent to the player at the moment.
+     * @return A dialogue object to be shown as text and have audio played
+     */
     public Dialogue getDialogue(){
         return dialouges.get(dialouges.size()-1);
     }
 
+    /**
+     * populates the dialogues list
+     * @param message message said to the player
+     * @param completionEvent string indicating when the message is no longer relevent
+     * @param id id number for sound file
+     */
     public void addDialogue(String message, String completionEvent, int id){
         Dialogue newDialogue = new Dialogue(message, completionEvent, this.name, id);
         dialouges.add(0, newDialogue);
